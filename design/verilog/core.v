749eef55b4039751716cbde5edb437aa0271f6a7
Shail Vaidya <shvaidya@ucsd.edu>
2024-11-22 23:53:48 -0800

    1.Added SRAM0 and SRAM1 2.Updated corelet.v and core.v with connections #6

--- design/verilog/core.v
+++ design/verilog/core.v
@@ -1,2 +1,101 @@
 //***************************************************************************
-// 
+// Top Level Design Module to be connected to Testbench as DUT
+
+// Consists of three main components
+// 1. Corelet.v (All the synthesizable logic)
+// 2. SRAM0 (For activation and weights)
+// 3. SRAM1 (For psum storing)
+//***************************************************************************
+module core #(
+    parameter bw = 4,
+    parameter psum_bw = 16,
+    parameter col = 8,
+    parameter row = 8
+) (
+    input                   clk,
+    input                   reset,
+    input                   ofifo_valid,
+    input   [bw*row-1:0]    D_xmem,
+    input   [33:0]          inst,
+    output                  sfp_out
+);
+
+//***********************************************
+//        Instruction Mapping
+//***********************************************
+//  inst[33]      = acc_q;
+//  inst[32]      = CEN_pmem_q;
+//  inst[31]      = WEN_pmem_q;
+//  inst[30:20]   = A_pmem_q;
+//  inst[19]      = CEN_xmem_q;
+//  inst[18]      = WEN_xmem_q;
+//  inst[17:7]    = A_xmem_q;
+//  inst[6]       = ofifo_rd_q;
+//  inst[5]       = ififo_wr_q;
+//  inst[4]       = ififo_rd_q;
+//  inst[3]       = l0_rd_q;
+//  inst[2]       = l0_wr_q;
+//  inst[1]       = execute_q; 
+//  inst[0]       = load_q; 
+
+//*************************************************************
+//                          Wires
+//*************************************************************
+wire [psum_bw*col-1:0] ofifo_rdata;
+wire [bw*row-1:0] l0_rdata;
+wire [bw*col-1:0] ififo_rdata;
+
+//*************************************************************
+//                      Corelet Instance
+//*************************************************************
+corelet #(
+    .bw (bw),
+    .psum_bw (psum_bw),
+    .col (col),
+    .row (row)
+) corelet_inst (
+    // Common Ports
+    .clk                (clk),
+    .reset              (reset),
+    // PE Array Ports
+    .inst_w             (inst[1:0]),
+    // OFIFO Ports
+    .ofifo_rd           (inst[6]),
+    .ofifo_valid        (ofifo_valid),
+    .ofifo_rdata        (ofifo_rdata),
+    // L0 Ports
+    .l0_rd              (inst[3]),
+    .l0_wr              (inst[2]),
+    .l0_wdata           (),
+    // IFIFO Ports
+    .ififo_rd           (inst[6]),
+    .ififo_wr           (inst[5]),
+    .ififo_wdata        (),
+    // SFP Ports
+    .sfp_out            (sfp_out)
+);
+
+//*************************************************************
+//                      SRAM0 Instance
+//*************************************************************
+SRAM0 SRAM0_inst (
+    .CLK    (clk),        
+    .WEN    (inst[18]),
+    .CEN    (inst[19]),
+    .D      (D_xmem),  
+    .A      (inst[17:7]),  
+    .Q      (l0_rdata)     //TODO muxing both IFIFO and L0 to SRAM0
+);
+//*************************************************************
+//                      SRAM1 Instance
+//*************************************************************
+SRAM1 SRAM1_inst (
+    .CLK    (clk),        
+    .WEN    (inst[31]),
+    .CEN    (inst[32]),
+    .D      (ofifo_rdata),  
+    .A      (inst[30:20]),  
+    .Q      ()     //TODO connecting SRAM1 to SFP
+);
+
+endmodule
--- design/verilog/corelet.v
+++ design/verilog/corelet.v
@@ -16,27 +16,50 @@ module corelet #(
     parameter row = 8
 ) (
     // Common Ports
-    input   clk,
-    input   reset,
+    input                       clk,
+    input                       reset,
     // PE Array Ports
-    input   in_n,
-    input   in_w,
-    input   inst_w,
+    input   [1:0]               inst_w,
     // OFIFO Ports
-    input   ofifo_rd,
-    output  ofifo_valid,
+    input                       ofifo_rd,
+    output                      ofifo_valid,
+    output  [psum*col-1:0]      ofifo_rdata,
     // L0 Ports
-    input   l0_rd,
-    input   l0_wr,
+    input                       l0_rd,
+    input                       l0_wr,
+    input   [bw*row-1:0]        l0_wdata,
     // IFIFO Ports
-    input   ififo_rd,
-    input   ififo_wr,
+    input                       ififo_rd,
+    input                       ififo_wr,
+    input   [bw*row-1:0]        ififo_wdata,
     // SFP Ports
-    output  sfu_out
+    output                      sfp_out
 );
 
 //*************************************************************
-//                  PE Array Instance
+//                             Wires
+//*************************************************************
+wire [col-1:0]          ofifo_wr;
+wire [psum_bw*col-1:0]  ofifo_wdata;
+wire [bw*col-1:0]       ififo_rdata;
+wire [psum_bw*col-1:0]  in_n;
+wire [bw*row-1:0]       l0_rdata;
+
+//*************************************************************
+//                      Stubbing (To be removed)
+//*************************************************************
+assign sfp_out = 1'b0;
+
+//*************************************************************
+//                      Misc  Logic
+//*************************************************************
+genvar i;
+    for (i=1; i < col ; i=i+1) begin
+        assign in_n[psum_bw*i+bw-1:psum_bw*i] = ififo_rdata[bw*(i+1)-1:bw*i];
+    end
+
+//*************************************************************
+//                      PE Array Instance
 //*************************************************************
 mac_array #(
     .bw (bw),
@@ -44,13 +67,13 @@ mac_array #(
     .col (col),
     .row (row)
 ) mac_array_inst (
-    .clk    (),     //TODO     
-    .reset  (),     //TODO
-    .out_s  (),     //TODO
-    .in_n   (),     //TODO
-    .in_w   (),     //TODO
-    .inst_w (),     //TODO
-    .valid  ()      //TODO
+    .clk    (clk),
+    .reset  (reset),
+    .out_s  (ofifo_wdata),
+    .in_n   (in_n),
+    .in_w   (l0_rdata),
+    .inst_w (inst_w),
+    .valid  (ofifo_wr)
 );
 
 //*************************************************************
@@ -64,8 +87,8 @@ l0 #(
     .reset      (reset),
     .wr         (l0_wr),
     .rd         (l0_rd),
-    .in         (),     //TODO
-    .out        (),     //TODO
+    .in         (l0_wdata),
+    .out        (l0_rdata),
     .o_full     (),     //TODO
     .o_ready    ()      //TODO
 );
@@ -81,8 +104,8 @@ l0 #(
     .reset      (reset),
     .wr         (ififo_wr),
     .rd         (ififo_rd),
-    .in         (),     //TODO
-    .out        (),     //TODO
+    .in         (ififo_wdata),
+    .out        (ififo_rdata),
     .o_full     (),     //TODO
     .o_ready    ()      //TODO
 );
@@ -96,10 +119,10 @@ ofifo #(
 ) ofifo_inst (
     .clk        (clk),
     .reset      (reset),
-    .wr         (),         //TODO
+    .wr         (ofifo_wr),
     .rd         (ofifo_rd),
-    .in         (),         //TODO
-    .out        (),         //TODO
+    .in         (ofifo_wdata),
+    .out        (ofifo_rdata),
     .o_full     (),         //TODO
     .o_ready    (),         //TODO
     .o_valid    (ofifo_valid)
