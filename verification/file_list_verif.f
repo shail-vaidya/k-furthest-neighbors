f97f57a3bdcdf074205f94ff67e097e9e687a5c2
Shail Vaidya <shvaidya@ucsd.edu>
2024-11-28 15:38:38 -0800

    Added verification filelist

---
+++ verification/file_list_verif.f
@@ -0,0 +1,16 @@
+../design/verilog/common_ip/fifo_mux_2_1.v
+../design/verilog/common_ip/fifo_mux_8_1.v
+../design/verilog/common_ip/fifo_mux_16_1.v
+../design/verilog/common_ip/fifo_depth8.v
+../design/verilog/common_ip/fifo_depth64.v
+../individual_files/Shail/l0.v
+../individual_files/Shail/ofifo.v
+../design/verilog/PE_array/mac.v
+../design/verilog/PE_array/mac_tile.v
+../design/verilog/PE_array/mac_row.v
+../design/verilog/PE_array/mac_array.v
+../design/verilog/corelet.v
+../design/verilog/memories/sram0_2048x32.v
+../design/verilog/memories/sram1_2048x128.v
+../design/verilog/core.v
+./core_tb.v
