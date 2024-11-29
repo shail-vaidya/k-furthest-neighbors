3b5e3ac9f71814e27e102a354c3b415ef391140e
Hariram-R <hari.2599@gmail.com>
2024-11-28 17:26:38 -0800

    sfu v1.1

--- design/verilog/PE_array/sfu.v
+++ design/verilog/PE_array/sfu.v
@@ -8,7 +8,7 @@ module sfu #(
     input reset,
     input acc_i,
     input [col*psum_bw-1:0] psum_in,
-    output valid_i,
+    output valid_o,
     output [col*psum_bw-1:0] psum_out
     );
 
@@ -19,17 +19,16 @@ module sfu #(
     wire [col*psum_bw-1:0] temp_psum_w;
     wire [col*psum_bw-1:0] temp_relu_psum_w;
 
-
-
-    assign psum_out = psum_q;
+    assign valid_o = acc_q && ~acc_i;
 
     genvar k;
     for (k=0;k<=col-1;k=k+1) begin
         // ReLU
         assign temp_psum_w[((k+1)*psum_bw)-1:k*psum_bw] = psum_q[((k+1)*psum_bw)-1:k*psum_bw] + psum_in[((k+1)*psum_bw)-1:k*psum_bw];
         //assign temp_psum_w = psum_q[(k+1)*psum_bw-1:k*psum_bw];
-        assign temp_relu_psum_w[((k+1)*psum_bw)-1:k*psum_bw] = temp_psum_w[((k+1)*psum_bw)-1] ? 0 : temp_psum_w[((k+1)*psum_bw)-1:k*psum_bw];         
+        assign temp_relu_psum_w[((k+1)*psum_bw)-1:k*psum_bw] = psum_q[((k+1)*psum_bw)-1] ? 0 : psum_q[((k+1)*psum_bw)-1:k*psum_bw];         
         assign psum_out[(k+1)*psum_bw-1:k*psum_bw] = temp_relu_psum_w[((k+1)*psum_bw)-1:k*psum_bw];
+        
     end
 
     integer j;
@@ -40,18 +39,16 @@ module sfu #(
             valid_q <= 0;
         end
         else begin
-            if(acc_q) begin
+            if(acc_i) begin
                 //for(j=0;j<=col-1;j=j+1) begin
                     //psum_q[((j+1)*psum_bw)-1:j*psum_bw] <= psum_q[((j+1)*psum_bw)-1:j*psum_bw] + psum_in[((j+1)*psum_bw)-1:j*psum_bw];
                 //end
                 psum_q <= temp_psum_w;
                 valid_q <= 1'b0;
             end
-            else if (acc_q && ~acc_i) begin
-                valid_q <= 1'b1;
-            end
             else begin
                 acc_q <= acc_i;
+                psum_q <= 0;
             end
         end
     end
