2c9c9cbb37b98ce0e1dc7d9e26aa21c2fbf8c502
Hariram-R <hari.2599@gmail.com>
2024-12-02 15:39:24 -0800

    sfu acc_out and sfu max_pool add

--- design/verilog/SFU/sfu_max_pool.v
+++ design/verilog/SFU/sfu_max_pool.v
@@ -1,4 +1,4 @@
-module sfu #( 
+module sfu_max_pool #( 
     parameter bw = 4,
     parameter psum_bw = 16,
     parameter col = 8,
@@ -10,13 +10,16 @@ module sfu #(
 
     input psum_bypass_i,
 
+    input max_pool_en_i,
+
     input [col*psum_bw-1:0] psum_in,
-    output [col*psum_bw-1:0] psum_out
+    output [col*psum_bw-1:0] psum_out,
+    output [(col/4)*psum_bw-1:0] max_pool_out
     );
 
     reg acc_q;
     //reg valid_q;
-    wire acc_out_q;
+    wire acc_out_w;
 
     reg [col*psum_bw-1:0] psum_q;
 
@@ -26,10 +29,16 @@ module sfu #(
     wire [col*psum_bw-1:0] temp_relu_only_psum_w;
     wire [col*psum_bw-1:0] temp_out_psum_w;
 
+    // 4 for MaxPool 2,2
+    wire [(col/4)*psum_bw-1:0] temp_out_max_pool1;
+    wire [(col/4)*psum_bw-1:0] temp_out_max_pool2; 
+    wire [(col/4)*psum_bw-1:0] temp_out_max_pool3; 
+    wire [(col/4)*psum_bw-1:0] a,b,c,d;
+
     assign acc_out_w = acc_q && ~acc_i;
-    
 
     genvar k;
+
     generate
         for (k=0;k<=col-1;k=k+1) begin : sfp_out_assign
             //Acc only
@@ -46,6 +55,21 @@ module sfu #(
             assign psum_out[((k+1)*psum_bw)-1:k*psum_bw] = psum_bypass_i ? psum_in[((k+1)*psum_bw)-1:k*psum_bw] : temp_out_psum_w[(k+1)*psum_bw-1:k*psum_bw];
 
         end
+
+        for (k=0;k<=(col/2);k=k+4) begin : sfp_max_pool_assign
+            assign a[(((k/4)+1)*psum_bw)-1:(k/4)*psum_bw] = psum_in[((k+1)*psum_bw)-1:k*psum_bw]; // 15:0
+            assign b[(((k/4)+1)*psum_bw)-1:(k/4)*psum_bw] = psum_in[((k+2)*psum_bw)-1:(k+1)*psum_bw]; //  31:16
+            assign c[(((k/4)+1)*psum_bw)-1:(k/4)*psum_bw] = psum_in[((k+3)*psum_bw)-1:(k+2)*psum_bw]; //  47:32
+            assign d[(((k/4)+1)*psum_bw)-1:(k/4)*psum_bw] = psum_in[((k+4)*psum_bw)-1:(k+3)*psum_bw]; //  63:48            
+
+            assign temp_out_max_pool1[(((k/4)+1)*psum_bw)-1:(k/4)*psum_bw] = (a[(((k/4)+1)*psum_bw)-1:(k/4)*psum_bw] > b[(((k/4)+1)*psum_bw)-1:(k/4)*psum_bw]) ? a[(((k/4)+1)*psum_bw)-1:(k/4)*psum_bw] : b[(((k/4)+1)*psum_bw)-1:(k/4)*psum_bw];
+            assign temp_out_max_pool2[(((k/4)+1)*psum_bw)-1:(k/4)*psum_bw] = (c[(((k/4)+1)*psum_bw)-1:(k/4)*psum_bw] > d[(((k/4)+1)*psum_bw)-1:(k/4)*psum_bw]) ? c[(((k/4)+1)*psum_bw)-1:(k/4)*psum_bw] : d[(((k/4)+1)*psum_bw)-1:(k/4)*psum_bw];
+
+            assign temp_out_max_pool3[(((k/4)+1)*psum_bw)-1:(k/4)*psum_bw] = (temp_out_max_pool1[(((k/4)+1)*psum_bw)-1:(k/4)*psum_bw] > temp_out_max_pool2[(((k/4)+1)*psum_bw)-1:(k/4)*psum_bw]) ? temp_out_max_pool1[(((k/4)+1)*psum_bw)-1:(k/4)*psum_bw] : temp_out_max_pool2[(((k/4)+1)*psum_bw)-1:(k/4)*psum_bw];
+
+            assign max_pool_out[(((k/4)+1)*psum_bw)-1:(k/4)*psum_bw] = temp_out_max_pool3[(((k/4)+1)*psum_bw)-1:(k/4)*psum_bw];
+        end
+
     endgenerate
     
 
@@ -54,7 +78,7 @@ module sfu #(
         if(reset) begin
             acc_q <= 0;
             psum_q <= 0;
-            //acc_out_q <= 0;
+            acc_out_q <= 0;
         end
         else begin
             acc_q <= acc_i;
